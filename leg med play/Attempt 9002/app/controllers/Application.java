c5784bc14cb114bcb5ec152ddb78778632f5f8e4
fumpen <frede_17@live.dk>
2015-05-04 12:20:47 +0200

    ny forbedret 9000

---
+++ leg med play/Attempt 9002/app/controllers/Application.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import models.Booking;
+import play.*;
+import play.data.Form;
+import play.db.ebean.Model;
+import play.mvc.*;
+
+import views.html.*;
+
+import java.util.List;
+
+import static play.libs.Json.toJson;
+
+public class Application extends Controller {
+
+    public static Result index() {
+        return ok(index.render());
+    }
+
+    public static Result gallery() {
+        return ok(Gallery.render());
+    }
+
+    public static Result bestil() {
+        return ok(Bestil.render());
+    }
+
+    public static Result addBooking(){
+        Booking booking = Form.form(Booking.class).bindFromRequest().get();
+        booking.save();
+        return redirect(routes.Application.bestil());
+    }
+
+    public static  Result getBookings(){
+        List<Booking> bookings = new Model.Finder(String.class, Booking.class).all();
+        return ok(toJson(bookings));
+    }
+
+}
---
+++ leg med play/Attempt 9002/conf/evolutions/default/1.sql
@@ -0,0 +1,28 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table booking (
+  id                        varchar(255) not null,
+  name                      varchar(255),
+  lname                     varchar(255),
+  dato                      varchar(255),
+  constraint pk_booking primary key (id))
+;
+
+create sequence booking_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists booking;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists booking_seq;
+
