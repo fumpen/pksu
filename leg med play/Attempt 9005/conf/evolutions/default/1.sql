f5a4a9062df6fe5ebb296c2b55a55c74b8e6b788
fumpen <frede_17@live.dk>
2015-06-01 16:29:26 +0200

    ny side igen igen igen igen igen

---
+++ leg med play/Attempt 9005/conf/evolutions/default/1.sql
@@ -0,0 +1,25 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table booking (
+  id                        integer primary key AUTOINCREMENT,
+  name                      varchar(255),
+  lname                     varchar(255),
+  arr_date                  varchar(255),
+  dep_date                  varchar(255),
+  room                      varchar(255))
+;
+
+
+
+
+# --- !Downs
+
+PRAGMA foreign_keys = OFF;
+
+drop table booking;
+
+PRAGMA foreign_keys = ON;
+
